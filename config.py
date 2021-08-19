9339d05882529ee2466e52074f48a0835fd82c82
alison2021 <75880099+alison2021@users.noreply.github.com>
2021-08-19 21:29:29 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-    APP_ID = int(os.environ.get("APP_ID", 1234))
-
-    API_HASH = os.environ.get("API_HASH", "")
