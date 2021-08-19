6e9556faeacde08cf97a94a0f0b7323679c5c291
alison2021 <75880099+alison2021@users.noreply.github.com>
2021-08-19 19:26:22 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -108,41 +108,6 @@ async def start(bot, update):
         disable_web_page_preview=True,
         reply_markup=reply_markup
     )
-
-      
-@bot.on_message(filters.media & filters.private)
-async def upload(client, message):
-    if Config.UPDATES_CHANNEL is not None:
-        try:
-            user = await client.get_chat_member(Config.UPDATES_CHANNEL, message.chat.id)
-            if user.status == "kicked":
-                await client.send_message(
-                    chat_id=message.chat.id,
-                    text="**Sᴏʀʀʏ, Yᴏᴜ ᴀʀᴇ Bᴀɴɴᴇᴅ ᴛᴏ ᴜsᴇ ᴍᴇ! Cᴏɴᴛᴀᴄᴛ** [Dᴇᴠᴇʟᴏᴘᴇʀ](https://telegram.me/AvishkarPatil).",
-                    parse_mode="markdown",
-                    disable_web_page_preview=False 
-                )
-                return
-        except UserNotParticipant:
-            await client.send_message(
-                chat_id=message.chat.id,
-                text="**Pʟᴇᴀsᴇ Jᴏɪɴ Mʏ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ Tᴏ Usᴇ Mᴇ 🏃‍♂**",
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton("Jᴏɪɴ Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ", url=f"https://t.me/{Config.UPDATES_CHANNEL}")
-                        ]
-                    ]
-                ),
-                parse_mode="markdown"
-            )
-            return
-        except Exception:
-            await client.send_message(
-                chat_id=message.chat.id,
-                text="**Sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ Wʀᴏɴɢ! Cᴏɴᴛᴀᴄᴛ ᴍʏ** [Dᴇᴠᴇʟᴏᴘᴇʀ](https://telegram.me/AvishkarPatil).",
-                parse_mode="markdown",
-                disable_web_page_preview=False)
             return
     m = await message.reply("**Dᴏᴡɴʟᴏᴀᴅɪɴɢ Yᴏᴜʀ FIʟᴇs Tᴏ Mʏ Sᴇʀᴠᴇʀ ....** ")
     now = time.time()
